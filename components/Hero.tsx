4c0d8ba04f81e304715df48f9d16cb47250cefa6
l-Oskar <fediuk_v@protonmail.com>
2025-05-26 13:25:53 +0300

    Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { FaLocationArrow } from "react-icons/fa6";
+import { cn } from "@/utils/cn";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagickButton from "./ui/MagickButton";
 
 const Hero = () => {
   return (
@@ -14,6 +18,42 @@ const Hero = () => {
           fill="purple"
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw" fill="blue" />
+      </div>
+      <div className="flex h-screen w-full dark:bg-black-100 items-center justify-center bg-white absolute top-0 left-0">
+        <div
+          className={cn(
+            // "relative",
+            "absolute inset-0",
+            "[background-size:22px_22px]",
+            "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
+            "dark:[background-image:radial-gradient(rgba(64,64,64,1)_1px,transparent_1px)]"
+          )}
+        />
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"></div>
+      </div>
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vh] md:max-w-2l lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless User Experiences"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m Volodymyr, a Next.js Developer
+          </p>
+
+          <a href="#about">
+            <MagickButton
+              title="Whow my works "
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
       </div>
     </div>
   );

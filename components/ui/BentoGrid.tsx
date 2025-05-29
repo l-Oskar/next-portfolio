0a4259e7322d3eb3e29ffbb38231830182a40203
l-Oskar <fediuk_v@protonmail.com>
2025-05-29 17:00:02 +0300

    Email Block

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,6 +1,12 @@
+"use client";
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { IoCopyOutline } from "react-icons/io5";
 import { GlobeDemo } from "@/components/ui/GridGlobe";
+import { useState } from "react";
+import animationData from "@/data/confetti.json";
+import Lottie from "react-lottie";
+import MagickButton from "./MagickButton";
 
 export const BentoGrid = ({
   className,
@@ -44,6 +50,23 @@ export const BentoGridItem = ({
   const leftLists = ["ReactJS", "NextJS", "Typescript"];
   const rightLists = ["Redux", "Express", "TailwindCSS"];
 
+  const [copied, setCopied] = useState(false);
+
+  const defaultOptions = {
+    loop: copied,
+    autoplay: copied,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText("fediuk_v@protonmail.com");
+
+    setCopied(true);
+  };
+
   return (
     <div
       className={cn(
@@ -55,7 +78,7 @@ export const BentoGridItem = ({
         backgroundColor:
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}>
-      <div className={`${id === 6} && "flex justify-center h-full"`}>
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
             <img
@@ -77,13 +100,14 @@ export const BentoGridItem = ({
         </div>
         {id === 6 && (
           <BackgroundGradientAnimation>
-            <div className="absolute z-50 flex items-center justify-center text-white font-bold" />
+            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
           </BackgroundGradientAnimation>
         )}
         <div
           className={cn(
             titleClassName,
-            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-8"
+            id !== 6 ? "group-hover/bento:translate-x-2" : "",
+            "transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-8"
           )}>
           <div className="font-sans font-extralight md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
             {description}
@@ -106,10 +130,10 @@ export const BentoGridItem = ({
                     {item}
                   </span>
                 ))}
-                <span className="lg:py-6 lg:px-3 py-4 px-3 rounded-lg text-center bg-[#10132E]"></span>
+                <span className="lg:py-6 lg:px-3 py-4 px-3 rounded-lg text-center bg-[#15193b]"></span>
               </div>
               <div className="flex flex-col gap-3 md:gap-3 lg:gap-3">
-                <span className="lg:py-4 lg:px-3 py-4 px-3 rounded-lg text-center bg-[#10132E]"></span>
+                <span className="lg:py-4 lg:px-3 py-4 px-3 rounded-lg text-center bg-[#15193b]"></span>
                 {rightLists.map((item, i) => (
                   <span
                     key={i}
@@ -119,6 +143,24 @@ export const BentoGridItem = ({
                   </span>
                 ))}
               </div>
+            </div>
+          )}
+
+          {id === 6 && (
+            <div className="mt-5 relative">
+              <div
+                className={`absolute -bottom-5 right-0 ${
+                  copied ? "block" : "block"
+                }`}>
+                <Lottie options={defaultOptions} height={200} width={400} />
+              </div>
+              <MagickButton
+                title={copied ? "Email copied" : "Copy my email"}
+                icon={<IoCopyOutline />}
+                position="left"
+                otherClasses="!bg-[#161a31]"
+                handleClick={handleCopy}
+              />
             </div>
           )}
         </div>

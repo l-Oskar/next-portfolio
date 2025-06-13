2ca028e59cf6dcb324d0ed55f9e79b8d4706f8e6
l-Oskar <fediuk_v@protonmail.com>
2025-06-13 16:07:28 +0300

    Fixed 2 blocks and information

--- data/index.tsx
+++ data/index.tsx
@@ -8,12 +8,13 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
+    title:
+      "I focus on clear communication and long-term collaboration to turn ideas into great digital products.",
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-    imgClassName: "w-full h-full",
+    imgClassName: "w-full h-full bg-black bg-opacity-50",
     titleClassName: "justify-end",
-    img: "/b1.svg",
+    img: "/laptop.jpg",
     spareImg: "",
   },
   {
@@ -38,7 +39,7 @@ export const gridItems = [
   },
   {
     id: 4,
-    title: "Tech enthusiast with a passion for development.",
+    title: "Building clean, modern web apps.",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
@@ -49,7 +50,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
+    title: "Currently building an open-source blockchain service.",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
